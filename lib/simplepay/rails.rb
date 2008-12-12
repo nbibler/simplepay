ba34f425988393809da77c3f8c0f9b0d1dbb2ecb
Nathaniel Bibler <git@nathanielbibler.com>
2008-12-11 19:32:23 -0500

    Updated README and added notifier to Rails helper.

--- lib/simplepay/rails.rb
+++ lib/simplepay/rails.rb
@@ -1,5 +1,9 @@
+require 'simplepay'
 require 'simplepay/helpers/rails_helper'
 require 'simplepay/helpers/notification_helper'
 
 # Inject helper into Rails ActionView.
 ActionView::Base.__send__(:include, Simplepay::Helpers::RailsHelper)
+
+# Inject notification helper into ActionController
+ActionController::Base.__send__(:include, Simplepay::Helpers::NotificationHelper)
