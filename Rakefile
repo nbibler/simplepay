88512ddef47bac5c5cc098372bf9bc2f65e35aba
Kurt Sussman <kls@merlot.com>
2010-11-29 13:45:36 -0800

    add test Rake task, require the necessary parts of active_support

--- Rakefile
+++ Rakefile
@@ -1,4 +1,4 @@
-%w[rubygems rake rake/clean fileutils].each { |f| require f }
+%w[rubygems rake rake/clean rake/testtask fileutils].each { |f| require f }
 #require File.dirname(__FILE__) + '/lib/simplepay'
 
 begin
@@ -9,10 +9,18 @@ begin
     p.email       = 'gem@nathanielbibler.com'
     p.summary = 'Amazon SimplePay helpers for Rails 3'
     p.files = Dir["*", "{lib}/**/*"]
+    p.add_dependency 'activesupport', '~>3.0.0'
   end
   Jeweler::GemcutterTasks.new
 rescue LoadError
   puts "Jeweler (or a dependency) not available. Install it with: sudo geminstall jeweler"
+end
+
+Rake::TestTask.new(:test) do |t|
+  t.pattern = 'test/**/test_*.rb'
+  t.ruby_opts << '-rubygems'
+  t.libs << 'test'
+  t.verbose = true
 end
 
 task :default => [:test]
--- lib/simplepay.rb
+++ lib/simplepay.rb
@@ -2,17 +2,28 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require 'rubygems'
-require 'active_support'
+begin
+  require 'active_support/hash_with_indifferent_access'
+  require 'active_support/core_ext/string/inflections'
+  require 'active_support/core_ext/object/blank'
+  require 'active_support/core_ext/module/attribute_accessors'
+  require 'active_support/core_ext/module/hash_keys'
+  require 'active_support/callbacks'
+  require 'active_support/inflector'
+rescue LoadError
+  # support older versions of active_support
+  require "active_support"
+end
 
 module Simplepay
   
   VERSION = '0.2.2' unless const_defined?(:VERSION)
 
-  attr_accessor :aws_access_key_id
-  attr_accessor :aws_secret_access_key
-  attr_accessor :account_id
+  mattr_accessor :aws_access_key_id
+  mattr_accessor :aws_secret_access_key
+  mattr_accessor :account_id
 
-  attr_accessor :use_sandbox
+  mattr_accessor :use_sandbox
   @@use_sandbox = true
 
   def self.use_sandbox?
