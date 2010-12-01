a393eab5ca00f988c9c37d79533395786faf6d72
Kurt Sussman <kls@merlot.com>
2010-11-30 19:42:25 -0800

    make form output readable for debugging

--- lib/simplepay/helpers/form_helper.rb
+++ lib/simplepay/helpers/form_helper.rb
@@ -4,11 +4,11 @@ module Simplepay
     module FormHelper
       
       def self.tag(name, options = nil, open = false)
-        "<#{name}#{tag_options(options) if options}" + (open ? ">" : " />")
+        "<#{name}#{tag_options(options) if options}" + (open ? ">\n" : " />\n")
       end
       
       def self.content_tag(name, content, options = nil)
-        "<#{name}#{tag_options(options)}>#{content}</#{name}>"
+        "<#{name}#{tag_options(options)}>#{content}</#{name}>\n"
       end
       
       
